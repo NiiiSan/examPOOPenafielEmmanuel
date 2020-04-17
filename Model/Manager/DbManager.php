b1d46558da5fda89d2f9590c1b9d42763943d8db
OtakuSama <51233051+NiiiSan@users.noreply.github.com>
2020-04-17 16:47:36 +0200

    j'ai pas trouvé

--- Model/Manager/DbManager.php
+++ Model/Manager/DbManager.php
@@ -11,7 +11,7 @@
         {
 
             try {
-                $this->bdd = new PDO('mysql:host='.$this->host.'; dbname='. $this->dbName , $this->username, $this->password);
+                $this->bdd = new PDO('mysql:host='.$this->host.' , dbname='. $this->dbName , $this->username, $this->password);
                 $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             catch(Exception $e) {
